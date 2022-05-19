63c88bfd836e2b9418ce98ced441c42e0c965e2a
Zachary Powell <zachary.powell@vonage.com>
2022-05-19 09:30:38 +0100

    Source code for flutter project

---
+++ app_to_phone/lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: CallWidget(title: 'app-to-phone-flutter'),
+    );
+  }
+}
+
+class CallWidget extends StatefulWidget {
+  const CallWidget({Key key = const Key("any_key"), required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _CallWidgetState createState() => _CallWidgetState();
+}
+
+class _CallWidgetState extends State<CallWidget> {
+  SdkState _sdkState = SdkState.LOGGED_OUT;
+  static const platformMethodChannel = MethodChannel('com.vonage');
+
+  _CallWidgetState() {
+    platformMethodChannel.setMethodCallHandler(methodCallHandler);
+  }
+
+  Future<dynamic> methodCallHandler(MethodCall methodCall) async {
+    switch (methodCall.method) {
+      case 'updateState':
+        {
+          setState(() {
+            var arguments = 'SdkState.${methodCall.arguments}';
+            _sdkState = SdkState.values.firstWhere((v) {return v.toString() == arguments;}
+            );
+          });
+        }
+        break;
+      default:
+        throw MissingPluginException('notImplemented');
+    }
+  }
+
+  Future<void> _loginUser() async {
+    String token = "ALICE_JWT";
+
+    try {
+      await platformMethodChannel
+          .invokeMethod('loginUser', <String, dynamic>{'token': token});
+    } on PlatformException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> _makeCall() async {
+    try {
+      await requestPermissions();
+
+      await platformMethodChannel.invokeMethod('makeCall');
+    } on PlatformException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> requestPermissions() async {
+    await [ Permission.microphone] .request();
+  }
+
+  Future<void> _endCall() async {
+    try {
+      await platformMethodChannel.invokeMethod('endCall');
+    } on PlatformException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 64),
+            _updateView()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _updateView() {
+    if (_sdkState == SdkState.LOGGED_OUT) {
+      return ElevatedButton(
+          onPressed: () { _loginUser(); },
+          child: const Text("LOGIN AS ALICE")
+      );
+    } else if (_sdkState == SdkState.WAIT) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (_sdkState == SdkState.LOGGED_IN) {
+      return ElevatedButton(
+          onPressed: () { _makeCall(); },
+          child: const Text("MAKE PHONE CALL")
+      );
+    } else if (_sdkState == SdkState.ON_CALL) {
+      return ElevatedButton(
+          onPressed: () { _endCall(); },
+          child: const Text("END CALL")
+      );
+    } else {
+      return const Center(
+          child: Text("ERROR")
+      );
+    }
+  }
+}
+
+enum SdkState {
+  LOGGED_OUT,
+  LOGGED_IN,
+  WAIT,
+  ON_CALL,
+  ERROR
+}
